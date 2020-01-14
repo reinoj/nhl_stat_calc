b7a6c52301cd69c9d0d0b846710e82d01bc5fffb
reinoj <penguinreincarnation@gmail.com>
2020-01-13 23:34:15 -0600

    Database and table creation are all under one flag now. Added more comments.

--- src/main.go
+++ src/main.go
@@ -15,16 +15,13 @@ func main() {
 
 	//---------------FLAGS---------------
 	// boolean flag for whether to create the database and tables
-	var createDatabaseFlag bool
-	//
-	var createTablesFlag bool
-	//
+	var initialSetupFlag bool
+	// string flag for the mysql user name
 	var mysqlUserFlag string
-	//
+	// string flag for the mysql password
 	var mysqlPasswordFlag string
 
-	flag.BoolVar(&createDatabaseFlag, "createDatabase", false, "create the database")
-	flag.BoolVar(&createTablesFlag, "createTables", false, "create the tables")
+	flag.BoolVar(&initialSetupFlag, "initialSetup", false, "create the database and base tables.")
 	flag.StringVar(&mysqlUserFlag, "mysqlUser", "root", "user name for mysql")
 	flag.StringVar(&mysqlPasswordFlag, "mysqlPassword", "root", "mysql for mysql user")
 
@@ -32,11 +29,12 @@ func main() {
 	flag.Parse()
 	//---------------FLAGS---------------
 
-	//---------------CREATE INITIAL DATABASE---------------
-	// Assigns the user name and password to the begininnign of the string
+	// Assigns the user name and password to the beginning of the string
 	mysqlSignIn := fmt.Sprintf("%s:%s", mysqlUserFlag, mysqlPasswordFlag)
-	// check createDatabaseFlag
-	if createDatabaseFlag {
+
+	// check initialSetupFlag
+	if initialSetupFlag {
+		//---------------CREATE INITIAL DATABASE---------------
 		fmt.Println("Opening initial database...")
 		db, err := sql.Open("mysql", mysqlSignIn+"@tcp(127.0.0.1:3306)/")
 		if err != nil {
@@ -47,30 +45,26 @@ func main() {
 		hockeydb.CreateDb(db)
 
 		db.Close()
-	}
-	//---------------CREATE INITIAL DATABASE---------------
+		//---------------CREATE INITIAL DATABASE---------------
 
-	//---------------CREATE TABLES---------------
-	fmt.Println("Opening Hockey database...")
-	hdb, err := sql.Open("mysql", mysqlSignIn+"@tcp(127.0.0.1:3306)/Hockey")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer hdb.Close()
-	fmt.Println("Hockey database Opened.")
+		//---------------CREATE TABLES---------------
+		fmt.Println("Opening Hockey database...")
+		hdb, err := sql.Open("mysql", mysqlSignIn+"@tcp(127.0.0.1:3306)/Hockey")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer hdb.Close()
+		fmt.Println("Hockey database Opened.")
 
-	// check createTablesFlag
-	if createTablesFlag {
 		// Creates the tables for the database
 		hockeydb.CreateTables(hdb)
 		// Populate the Teams table
-		//hockeydb.GetTeams(hdb)
+		hockeydb.GetTeams(hdb)
 		// Populate the Schedule table
 		hockeydb.GetSchedule(hdb)
+		//---------------CREATE TABLES---------------
 	}
-	//---------------CREATE TABLES---------------
 
-	//hockeydb.GetSchedule(hdb)
 	fmt.Println("Hockey database closed.")
 	fmt.Println("Complete.")
 }
