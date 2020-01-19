428bc45747ea75466597c14eba7b028a44aca0d1
reinoj <penguinreincarnation@gmail.com>
2020-01-18 23:55:10 -0600

    Commented out unneeded parts of the struct.

--- src/hockeydb/schedule_json.go
+++ src/hockeydb/schedule_json.go
@@ -1,30 +1,30 @@
 package hockeydb
 
-type state struct {
+/*type state struct {
 	AbstractGameState string `json:"abstractGameState"`
 	CodedGameState    string `json:"codedGameState"`
 	DetailedState     string `json:"detailedState"`
 	StatusCode        string `json:"statusCode"`
 	StartTimeTBD      bool   `json:"startTimeTBD"`
-}
+}*/
 
-type leagueRecord struct {
+/*type leagueRecord struct {
 	Wins   uint8  `json:"wins"`
 	Losses uint8  `json:"losses"`
 	OT     uint8  `json:"ot"`
 	Type   string `json:"type"`
-}
+}*/
 
 type teamDescription struct {
-	ID   uint8  `json:"id"`
+	// ID uint8 `json:"id"`
 	Name string `json:"name"`
-	Link string `json:"link"`
+	// Link string `json:"link"`
 }
 
 type aHTeam struct {
-	Record leagueRecord    `json:"leagueRecord"`
-	Score  uint8           `json:"score"`
-	Team   teamDescription `json:"team"`
+	// Record leagueRecord    `json:"leagueRecord"`
+	// Score  uint8           `json:"score"`
+	Team teamDescription `json:"team"`
 }
 
 type awayHome struct {
@@ -32,46 +32,46 @@ type awayHome struct {
 	Home aHTeam `json:"home"`
 }
 
-type venueInfo struct {
+/*type venueInfo struct {
 	ID   uint16 `json:"id"`
 	Name string `json:"name"`
 	Link string `json:"link"`
-}
+}*/
 
-type gameContent struct {
+/*type gameContent struct {
 	Link string `json:"link"`
-}
+}*/
 
 type game struct {
-	GamePK   uint32      `json:"gamePk"`
-	Link     string      `json:"link"`
-	GameType string      `json:"gameType"`
-	Season   string      `json:"season"`
-	GameDate string      `json:"gameDate"`
-	Status   state       `json:"status"`
-	Teams    awayHome    `json:"teams"`
-	Venue    venueInfo   `json:"venue"`
-	Content  gameContent `json:"content"`
+	GamePK uint32 `json:"gamePk"`
+	// Link     string      `json:"link"`
+	GameType string `json:"gameType"`
+	// Season   string      `json:"season"`
+	// GameDate string      `json:"gameDate"`
+	// Status   state       `json:"status"`
+	Teams awayHome `json:"teams"`
+	// Venue    venueInfo   `json:"venue"`
+	// Content  gameContent `json:"content"`
 }
 
 type date struct {
-	Date         string   `json:"date"`
-	TotalItems   uint8    `json:"totalItems"`
-	TotalEvents  uint8    `json:"totalEvents"`
-	TotalGames   uint8    `json:"totalGames"`
-	TotalMatches uint8    `json:"totalMatches"`
-	Games        []game   `json:"games"`
-	Events       []string `json:"events"`
-	Matches      []string `json:"matches"`
+	Date string `json:"date"`
+	// TotalItems   uint8    `json:"totalItems"`
+	// TotalEvents  uint8    `json:"totalEvents"`
+	TotalGames uint8 `json:"totalGames"`
+	// TotalMatches uint8    `json:"totalMatches"`
+	Games []game `json:"games"`
+	// Events       []string `json:"events"`
+	// Matches      []string `json:"matches"`
 }
 
 // Schedule is used for holding the json from NHL schedule statsapi
 type Schedule struct {
-	Copyright    string `json:"copyright"`
-	TotalItems   uint16 `json:"totalItems"`
-	TotalEvents  uint8  `json:"totalEvents"`
-	TotalGames   uint16 `json:"totalGames"`
-	TotalMatches uint8  `json:"totalMatches"`
-	Wait         int8   `json:"wait"`
-	Dates        []date `json:"dates"`
+	// Copyright    string `json:"copyright"`
+	// TotalItems   uint16 `json:"totalItems"`
+	// TotalEvents  uint8  `json:"totalEvents"`
+	// TotalGames   uint16 `json:"totalGames"`
+	// TotalMatches uint8  `json:"totalMatches"`
+	// Wait         int8   `json:"wait"`
+	Dates []date `json:"dates"`
 }
--- src/hockeydb/team_json.go
+++ src/hockeydb/team_json.go
@@ -1,58 +1,58 @@
 package hockeydb
 
-type timezone struct {
+/*type timezone struct {
 	ID     string `json:"id"`
 	Offset int8   `json:"offset"`
 	TZ     string `json:"tz"`
-}
+}*/
 
-type venue struct {
+/*type venue struct {
 	Name     string   `json:"name"`
 	Link     string   `json:"link"`
 	City     string   `json:"city"`
 	TimeZone timezone `json:"timeZone"`
-}
+}*/
 
 type division struct {
-	ID           uint8  `json:"id"`
-	Name         string `json:"name"`
-	NameShort    string `json:"nameShort"`
-	Link         string `json:"link"`
-	Abbreviation string `json:"abbreviation"`
+	// ID           uint8  `json:"id"`
+	Name string `json:"name"`
+	// NameShort    string `json:"nameShort"`
+	// Link         string `json:"link"`
+	// Abbreviation string `json:"abbreviation"`
 }
 
 type conference struct {
-	ID   uint8  `json:"id"`
+	// ID   uint8  `json:"id"`
 	Name string `json:"name"`
-	Link string `json:"link"`
+	// Link string `json:"link"`
 }
 
-type franchise struct {
+/*type franchise struct {
 	FranchiseID uint8  `json:"franchiseId"`
 	TeamName    string `json:"teamName"`
 	Link        string `json:"link"`
-}
+}*/
 
 type team struct {
-	ID              uint8      `json:"id"`
-	Name            string     `json:"name"`
-	Link            string     `json:"link"`
-	Venue           venue      `json:"venue"`
-	Abbreviation    string     `json:"abbreviation"`
-	TeamName        string     `json:"teamName"`
-	LocationName    string     `json:"locationName"`
-	FirstYearOfPlay string     `json:"firstYearOfPlay"`
-	Division        division   `json:"division"`
-	Conference      conference `json:"conference"`
-	Franchise       franchise  `json:"franchise"`
-	ShortName       string     `json:"shortName"`
-	OfficialSiteURL string     `json:"officialSiteUrl"`
-	FranchiseID     uint8      `json:"franchiseId"`
-	Active          bool       `json:"active"`
+	ID   uint8  `json:"id"`
+	Name string `json:"name"`
+	// Link            string     `json:"link"`
+	// Venue           venue      `json:"venue"`
+	Abbreviation string `json:"abbreviation"`
+	TeamName     string `json:"teamName"`
+	LocationName string `json:"locationName"`
+	// FirstYearOfPlay string     `json:"firstYearOfPlay"`
+	Division   division   `json:"division"`
+	Conference conference `json:"conference"`
+	// Franchise       franchise  `json:"franchise"`
+	// ShortName       string     `json:"shortName"`
+	// OfficialSiteURL string     `json:"officialSiteUrl"`
+	// FranchiseID     uint8      `json:"franchiseId"`
+	// Active          bool       `json:"active"`
 }
 
 // currentTeams stores the teams json from the NHL statsapi
 type currentTeams struct {
-	Copyright string `json:"copyright"`
-	Teams     []team `json:"teams"`
+	// Copyright string `json:"copyright"`
+	Teams []team `json:"teams"`
 }
